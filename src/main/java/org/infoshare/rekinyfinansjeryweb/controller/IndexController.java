58da69605f3c2ec01037c24ecee2839052e95df9
Paulina Wojcik <pinawoj@gmail.com>
2022-04-19 20:48:09 +0200

    Main page

--- src/main/java/org/infoshare/rekinyfinansjeryweb/controller/IndexController.java
+++ src/main/java/org/infoshare/rekinyfinansjeryweb/controller/IndexController.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/template")
-public class TemplateController {
+@RequestMapping("/")
+public class IndexController {
 
     @GetMapping
-    public String getTemplate() {
-        return "template";
+    public String getIndex() {
+        return "index";
     }
-
 }
