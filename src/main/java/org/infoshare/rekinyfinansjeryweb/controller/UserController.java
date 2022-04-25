63956bfdf52f8377c0456ab85677299a0338bd8f
Maciej Chyla <keicam18@poczta.fm>
2022-04-26 00:18:52 +0200

    change of login controller

--- src/main/java/org/infoshare/rekinyfinansjeryweb/controller/UserController.java
+++ src/main/java/org/infoshare/rekinyfinansjeryweb/controller/UserController.java
@@ -2,22 +2,29 @@ package org.infoshare.rekinyfinansjeryweb.controller;
 
 import org.infoshare.rekinyfinansjeryweb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("user")
 public class UserController {
 
     @Autowired
     UserService usersService;
+
     @GetMapping
-    public String getUser() {
-        if (usersService.getUser().getId() == 0) {
-            return "login";
-        }
+    @Secured({"ROLE_USER", "ROLE_ADMIN"})
+    public String getUser(Model model) {
+        usersService.loginUser("admin@admin.pl","admin");
+        model.addAttribute("user", usersService.getUser());
+
+//        if (usersService.getUser().getId() == 0) {
+//            return "login";
+//        }
         return "user";
     }
 }
