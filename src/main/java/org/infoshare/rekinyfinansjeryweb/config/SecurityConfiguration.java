166f0058802d06e7f611a528978ef9e5f1c2ee92
Maciej Chyla <keicam18@poczta.fm>
2022-05-07 14:33:24 +0200

    added translation in login.html

--- src/main/java/org/infoshare/rekinyfinansjeryweb/config/SecurityConfiguration.java
+++ src/main/java/org/infoshare/rekinyfinansjeryweb/config/SecurityConfiguration.java
@@ -17,7 +17,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/styles/**", "/assets/**", "/js/**", "/tables", "/search", "/signup").permitAll()
+                .antMatchers("/", "/styles/**", "/assets/**", "/js/**", "/tables", "/search", "/signup", "/login").permitAll()
                 .antMatchers("/admin-panel").hasAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated()
                 .and()
