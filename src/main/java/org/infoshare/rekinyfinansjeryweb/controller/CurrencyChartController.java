bfb00efc41d457f29b64b93de5f71285a15a5276
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2022-05-07 13:28:54 +0200

    CurrencyChartController and chart.html modified, chart.html now displaying currency values from last 3 months

--- src/main/java/org/infoshare/rekinyfinansjeryweb/controller/CurrencyChartController.java
+++ src/main/java/org/infoshare/rekinyfinansjeryweb/controller/CurrencyChartController.java
@@ -4,10 +4,7 @@ import com.infoshareacademy.domain.DailyExchangeRates;
 import com.infoshareacademy.services.DailyExchangeRatesFiltrationService;
 import com.infoshareacademy.services.ExchangeRatesFiltrationService;
 import com.infoshareacademy.services.NBPApiManager;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import org.apache.tomcat.jni.Local;
-import org.springframework.data.repository.query.Param;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,13 +17,13 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/currency")
+@RequiredArgsConstructor
 public class CurrencyChartController {
 
     @GetMapping("/{code}")
     public String showChart(@PathVariable("code") String code) {
 
        List<ChartData> chartData = getChartData(code);
-
        chartData.forEach(System.out::println);
 
         return "chart";
@@ -36,33 +33,32 @@ public class CurrencyChartController {
     public String showHistory(@PathVariable("code") String code) {
 
         List<ChartData> chartData = getChartData(code);
-
         chartData.forEach(System.out::println);
 
         return "history";}
 
-    private List<ChartData> getChartData(String code){
+    public List<ChartData> getChartData(String code){
         DailyExchangeRatesFiltrationService ratesCollectionProvider = NBPApiManager.getInstance().getDailyExchangeRatesService();
 
         List<DailyExchangeRates> exchangeRateTablesForOneCurrency = ratesCollectionProvider.forEachDay(dailyExchangeRates -> new ExchangeRatesFiltrationService(dailyExchangeRates.
                 getRates()).
                 filterByShortName(Arrays.asList(code))).getDailyExchangeRates();
 
-        return exchangeRateTablesForOneCurrency.stream().map(table -> {
-            return new ChartData(table.getEffectiveDate(),
-                    table.getTradingDate(),
-                    table.getRates().get(0).getAsk(),
-                    table.getRates().get(0).getBid());}
+
+        return exchangeRateTablesForOneCurrency.stream().map(table -> new ChartData(table.getEffectiveDate(),
+                table.getTradingDate(),
+                table.getRates().get(0).getAsk(),
+                table.getRates().get(0).getBid())
         ).collect(Collectors.toList());
     }
 
-    @Data
-    @AllArgsConstructor
-    public class ChartData {
-        private LocalDate effectiveDate;
-        private LocalDate tradingDate;
-        private Double askPrice;
-        private Double bidPrice;
+    public record ChartData(
+            LocalDate effectiveDate,
+            LocalDate tradingDate,
+            Double askPrice,
+            Double bidPrice
+    ) {
+        //empty
     }
 
 }
