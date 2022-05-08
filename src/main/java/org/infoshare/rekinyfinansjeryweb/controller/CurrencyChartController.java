3560f88f6c2cc790fbd4c3fb004e1011305182c4
Zbyszek Bulecki <zbyszek.bulecki@gmail.com>
2022-05-08 19:02:22 +0200

    CurrencyChartController.html, chart.html, history.html modified to used models, createChartDataSet method added

--- src/main/java/org/infoshare/rekinyfinansjeryweb/controller/CurrencyChartController.java
+++ src/main/java/org/infoshare/rekinyfinansjeryweb/controller/CurrencyChartController.java
@@ -7,6 +7,7 @@ import com.infoshareacademy.services.NBPApiManager;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,27 +23,31 @@ import java.util.stream.Collectors;
 public class CurrencyChartController {
 
     @GetMapping("/{code}")
-    public String showChart(@PathVariable("code") String code) {
+    public String showChart(@PathVariable("code") String code, Model model) {
 
        List<ChartData> chartData = getChartData(code);
 //       chartData.forEach(System.out::println);
+        model.addAttribute("chartData", chartData);
+        return "chart";
+    }
 
+    @GetMapping("/{code}/{year}/{month}")
+    public String showChart(@PathVariable("code") String code, @PathVariable("year") int year, @PathVariable("month") int month, Model model) {
+
+        List<ChartData> chartData = getChartData(code, year, month);
+        model.addAttribute("chartData", chartData);
         return "chart";
     }
 
     @GetMapping("/history/{code}")
-    public String showHistory(@PathVariable("code") String code) {
+    public String showHistory(@PathVariable("code") String code, Model model) {
 
         List<ChartData> chartData = getChartData(code);
-        System.out.println(chartData.get(0).effectiveDate);
-        chartData.forEach(System.out::println);
-
+        model.addAttribute("chartData", chartData);
         return "history";
     }
 
-    public List<ChartData> getChartData(String code){
-        DailyExchangeRatesFiltrationService ratesCollectionProvider = NBPApiManager.getInstance().getDailyExchangeRatesService();
-
+    private List<ChartData> createChartDataSet(DailyExchangeRatesFiltrationService ratesCollectionProvider, String code){
         List<DailyExchangeRates> exchangeRateTablesForOneCurrency = ratesCollectionProvider.forEachDay(dailyExchangeRates -> new ExchangeRatesFiltrationService(dailyExchangeRates.
                 getRates()).
                 filterByShortName(Arrays.asList(code))).getDailyExchangeRates();
@@ -53,6 +58,19 @@ public class CurrencyChartController {
                 table.getRates().get(0).getAsk(),
                 table.getRates().get(0).getBid())
         ).collect(Collectors.toList());
+    }
+
+    public List<ChartData> getChartData(String code, int year, int month){
+        DailyExchangeRatesFiltrationService ratesCollectionProvider = NBPApiManager.getInstance().getDailyExchangeRatesService();
+        ratesCollectionProvider
+                .filterByTradingDateFrom(LocalDate.of(year, month, 1))
+                .filterByTradingDateTo(LocalDate.of(year, month, LocalDate.of(year, month, 1).lengthOfMonth()));
+        return createChartDataSet(ratesCollectionProvider, code);
+    }
+
+    public List<ChartData> getChartData(String code){
+        DailyExchangeRatesFiltrationService ratesCollectionProvider = NBPApiManager.getInstance().getDailyExchangeRatesService();
+        return createChartDataSet(ratesCollectionProvider, code);
     }
 
     public record ChartData(
