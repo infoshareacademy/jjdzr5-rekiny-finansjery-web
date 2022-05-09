4a3c213d19ada23114613c629c37dcf9f43a6628
Filip Bąkowski <filipbakowski5@gmail.com>
2022-05-09 16:02:43 +0200

    Added single table view controller

---
+++ src/main/java/org/infoshare/rekinyfinansjeryweb/controller/TableController.java
@@ -0,0 +1,31 @@
+package org.infoshare.rekinyfinansjeryweb.controller;
+
+import com.infoshareacademy.domain.DailyExchangeRates;
+import com.infoshareacademy.domain.ExchangeRate;
+import com.infoshareacademy.services.NBPApiManager;
+import org.infoshare.rekinyfinansjeryweb.form.TableSettings;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/table")
+public class TableController {
+    @GetMapping("/{no}")
+    public String singleTableView(@PathVariable("no") String no, Model model){
+        Optional<DailyExchangeRates> dailyExchangeRates = NBPApiManager.getInstance().findDailyTable(no.replaceAll("_", "/"));
+        if(dailyExchangeRates.isPresent()){
+            model.addAttribute("exchangeRates", dailyExchangeRates.get());
+        }
+        else{
+            model.addAttribute("exchangeRates", null);
+        }
+        model.addAttribute("newDailyTable", new TableSettings());
+        model.addAttribute("newCurrency", new ExchangeRate());
+        return "tableView";
+    }
+}
