65a8452b8a1a4f217acf06ec42eb4c60c25ec1ea
Zbyszek Bulecki <zbyszek.bulecki@gmail.com>
2022-05-20 19:40:16 +0200

    chart script added to index.html, ChartService created, CurrencyChartController updated to use ChartService, IndexController updated to use ChartService, possibility of choosing currencies displayed by index.html added

--- src/main/java/org/infoshare/rekinyfinansjeryweb/controller/IndexController.java
+++ src/main/java/org/infoshare/rekinyfinansjeryweb/controller/IndexController.java
@@ -7,7 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -17,12 +20,25 @@ public class IndexController {
     @Autowired
     ChartService chartService;
 
-    String[] currencyCodes = {"GBP", "USD", "EUR", "CHF", "KRU"};
+    List<String> currencies = new ArrayList<>(Arrays.asList("GBP", "USD", "EUR", "CHF"));
 
     @GetMapping
     public String showChart(Model model) {
 
-        List<ChartService.ChartData> chartData = chartService.getChartData(currencyCodes[0]);
+        model.addAttribute("currencies", currencies);
+        model.addAttribute("code", "EUR");
+
+        List<ChartService.ChartData> chartData = chartService.getChartData("EUR");
+        model.addAttribute("chartData", chartData);
+        return "index";
+    }
+
+    @GetMapping("/{code}")
+    public String showChart(@PathVariable String code, Model model) {
+
+        model.addAttribute("currencies", currencies);
+
+        List<ChartService.ChartData> chartData = chartService.getChartData(code);
         model.addAttribute("chartData", chartData);
         return "index";
     }
