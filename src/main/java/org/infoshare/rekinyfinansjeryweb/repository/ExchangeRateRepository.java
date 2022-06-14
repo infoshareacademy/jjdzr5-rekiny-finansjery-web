268c0e8b6bdd0ac67445e2063503871e1352be35
Filip Bąkowski <filipbakowski5@gmail.com>
2022-06-14 19:59:15 +0200

    Refactor of single daily table view

--- src/main/java/org/infoshare/rekinyfinansjeryweb/repository/ExchangeRateRepository.java
+++ src/main/java/org/infoshare/rekinyfinansjeryweb/repository/ExchangeRateRepository.java
@@ -1,6 +1,7 @@
 package org.infoshare.rekinyfinansjeryweb.repository;
 
 import org.infoshare.rekinyfinansjeryweb.entity.ExchangeRate;
+import org.infoshare.rekinyfinansjeryweb.entity.ExchangeRateCurrency;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +15,7 @@ public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long
     Optional<Long> countExchangeRatesFromPeriod(LocalDate from, LocalDate to);
     @Query("select er.date from ExchangeRate er where (:from is null or er.date>=:from) and (:to is null or er.date<=:to) group by er.date order by er.date desc")
     List<LocalDate> findExchangeRatesFromPeriod(LocalDate from, LocalDate to, Pageable pageable);
+    @Query("select new org.infoshare.rekinyfinansjeryweb.entity.ExchangeRateCurrency(er.id, er.date, er.askPrice, er.bidPrice, c.code, c.name, c.category) " +
+            "from ExchangeRate er join er.currency c where er.date = :selectedDate order by c.code")
+    List<ExchangeRateCurrency> findExchangeRatesByDate(LocalDate selectedDate);
 }
