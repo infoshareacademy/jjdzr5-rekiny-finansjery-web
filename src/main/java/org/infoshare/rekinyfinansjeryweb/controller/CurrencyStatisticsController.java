d8a838eec039ab623d30d105e11a41dbf8564c91
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2022-06-25 13:46:46 +0200

    missing methods added in the CurrencyStatisticsClientService and missing mapping added in CurrencyStatisticsController

--- src/main/java/org/infoshare/rekinyfinansjeryweb/controller/CurrencyStatisticsController.java
+++ src/main/java/org/infoshare/rekinyfinansjeryweb/controller/CurrencyStatisticsController.java
@@ -5,9 +5,8 @@ import org.infoshare.rekinyfinansjeryweb.service.CurrencyStatisticsClientService
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/stats")
@@ -16,11 +15,36 @@ public class CurrencyStatisticsController {
     @Autowired
     CurrencyStatisticsClientService currencyStatisticsClientService;
 
+    @RequestMapping("/all")
+    public String showAllStats(Model model){
+        model.addAttribute("results", currencyStatisticsClientService.getAllResults());
+        return "stats";
+    }
+
     @RequestMapping()
-    public String showStats(Model model){
-        currencyStatisticsClientService.increaseCount(List.of("USD"));
-        model.addAttribute("results", currencyStatisticsClientService.getResults());
+    public String showRecentStats(Model model){
+        model.addAttribute("results", currencyStatisticsClientService.getRecentResults());
+        return "stats";
+    }
+
+    @RequestMapping("currency/{code}")
+            public String showCurrencyStatsForCurrency(Model model, @PathVariable String code){
+        model.addAttribute("results", currencyStatisticsClientService.getOneResultByCode(code));
+        return "stats";
+    }
+
+    @RequestMapping("currency/{month}/{year}")
+    public String showCurrencyStatsforSelectedMonth(Model model, @PathVariable int month, @PathVariable int year){
+        model.addAttribute("results", currencyStatisticsClientService.getOneResultByMonthAndYear(month, year));
+        return "stats";
+    }
+
+    @RequestMapping("currency/{month}/{year}/{code}")
+    public String showSingleCurrencyStatsFromSelectedMonthForCurrency(Model model, @PathVariable int month, @PathVariable int year, @PathVariable String code){
+        model.addAttribute("results", currencyStatisticsClientService.getOneResultByCodeAndMonthAndYear(month, year, code));
         return "stats";
     }
 
 }
+
+//    currencyStatisticsClientService.increaseCount(List.of("USD"));
--- src/main/java/org/infoshare/rekinyfinansjeryweb/service/CurrencyStatisticsClientService.java
+++ src/main/java/org/infoshare/rekinyfinansjeryweb/service/CurrencyStatisticsClientService.java
@@ -19,12 +19,44 @@ public class CurrencyStatisticsClientService {
     @Autowired
     RestTemplate restTemplate;
 
-    public List <CurrencyStatisticsDTO> getResults(){
+    public List <CurrencyStatisticsDTO> getAllResults(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity <CurrencyStatisticsDTO[]> entity = new HttpEntity<CurrencyStatisticsDTO[]>(headers);
+
+        return Arrays.stream(restTemplate.exchange("http://localhost:8081/api/all", HttpMethod.GET, entity, CurrencyStatisticsDTO[].class).getBody()).toList();
+    }
+
+    public List <CurrencyStatisticsDTO> getRecentResults(){
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity <CurrencyStatisticsDTO[]> entity = new HttpEntity<CurrencyStatisticsDTO[]>(headers);
 
         return Arrays.stream(restTemplate.exchange("http://localhost:8081/api", HttpMethod.GET, entity, CurrencyStatisticsDTO[].class).getBody()).toList();
+    }
+
+    public List <CurrencyStatisticsDTO> getOneResultByCode(String currency){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity <CurrencyStatisticsDTO[]> entity = new HttpEntity<CurrencyStatisticsDTO[]>(headers);
+
+        return Arrays.stream(restTemplate.exchange("http://localhost:8081/api/currency/" + currency, HttpMethod.GET, entity, CurrencyStatisticsDTO[].class).getBody()).toList();
+    }
+
+    public List <CurrencyStatisticsDTO> getOneResultByMonthAndYear(int month, int year){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity <CurrencyStatisticsDTO[]> entity = new HttpEntity<CurrencyStatisticsDTO[]>(headers);
+
+        return Arrays.stream(restTemplate.exchange("http://localhost:8081/api/history/" + month + "/" + year, HttpMethod.GET, entity, CurrencyStatisticsDTO[].class).getBody()).toList();
+    }
+
+    public List <CurrencyStatisticsDTO> getOneResultByCodeAndMonthAndYear(int month, int year, String currency){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity <CurrencyStatisticsDTO[]> entity = new HttpEntity<CurrencyStatisticsDTO[]>(headers);
+
+        return Arrays.stream(restTemplate.exchange("http://localhost:8081/api/history/" + month + "/" + year + "/" + currency, HttpMethod.GET, entity, CurrencyStatisticsDTO[].class).getBody()).toList();
     }
 
     public void increaseCount(List <String> incrementedCurrencies){
@@ -37,4 +69,6 @@ public class CurrencyStatisticsClientService {
         restTemplate.exchange(
                 "http://localhost:8081/api/increment", HttpMethod.POST, entity, SearchedCurrenciesListDTO.class).getBody();
     }
+
+
 }
