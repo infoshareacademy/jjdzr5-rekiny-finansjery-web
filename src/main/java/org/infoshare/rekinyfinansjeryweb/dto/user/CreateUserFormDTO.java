cbe4d3e21f7265797a22300f1e3f836391981e17
Maciej Chyla <keicam18@poczta.fm>
2022-07-08 19:44:42 +0200

    added Repeat password to register form

--- src/main/java/org/infoshare/rekinyfinansjeryweb/dto/user/CreateUserFormDTO.java
+++ src/main/java/org/infoshare/rekinyfinansjeryweb/dto/user/CreateUserFormDTO.java
@@ -12,8 +12,10 @@ public class CreateUserFormDTO {
     @Email(message = "{validation.email}")
     @NotBlank(message = "{validation.email.blank}")
     private String email;
-    @Size(min = 8, max = 255, message = "{validation.password}")
+    @Size(min = 8, max = 32, message = "{validation.password}")
     private String password;
+    @Size(min = 8, max = 32, message = "{validation.password}")
+    private String repeatPassword;
     @Size(min = 3, max = 30, message = "{validation.name}")
     private String name;
     @Size(min = 3, max = 30, message = "{validation.lastname}")
